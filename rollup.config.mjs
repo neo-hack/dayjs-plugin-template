e5f024438dd7c32023853a2c80f3d9839b86cd83
JW <jiangweixian1994@gmail.com>
2023-09-03 20:14:48 +0800

    :construction_worker:  use esbuild

---
+++ rollup.config.mjs
@@ -0,0 +1,36 @@
+import alias from '@rollup/plugin-alias'
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import { defineConfig } from 'rollup'
+import esbuild from 'rollup-plugin-esbuild'
+import { externals } from 'rollup-plugin-node-externals'
+
+export default defineConfig([
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // an array for the `output` option, where we can specify
+  // `file` and `format` for each target)
+  {
+    input: 'src/index.ts',
+    plugins: [
+      externals({
+        devDeps: false,
+      }),
+      resolve(),
+      esbuild({
+        target: 'node16',
+      }), // so Rollup can convert TypeScript to JavaScript
+      alias({
+        resolve: ['.ts', '.js', '.tsx', '.jsx'],
+        entries: [{ find: '@/', replacement: './src/' }],
+      }),
+      commonjs(),
+    ],
+    output: [
+      { format: 'cjs', dir: 'dist', entryFileNames: '[name].cjs' },
+      { format: 'es', dir: 'dist', entryFileNames: '[name].mjs' },
+    ],
+  },
+])
